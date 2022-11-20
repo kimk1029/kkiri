166af61e93c881564282250b91bab17a857ce9b9
JKJeon <eworud@gmail.com>
2022-11-20 20:08:02 +0900

    db셋팅 postgresql 설치 env 설정

--- backend/README.md
+++ backend/README.md
@@ -71,3 +71,6 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+
+## envSample.txt 해당 파일 .env로 변경 필요
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,55 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule,ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        //const dbType = config.get('DB_TYPE');
+        const dbType: string = process.env.DB_TYPE;
+        console.log("appModule");
+        console.log(dbType);
+        const option = {
+          database: config.get('DB_DATABASE'),
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: Boolean(config.get('DB_SYNC')),
+          logging: Boolean(config.get('DB_LOG')),
+        };
+        switch (dbType) {
+          case 'sqlite':
+            return { ...option, type: 'sqlite' };
+          case 'postgres':
+            return {
+              ...option,
+              type: 'postgres',
+              host: config.get('DB_HOST'),
+              port: +config.get('DB_PORT'),
+              username: config.get('DB_USERNAME'),
+              password: config.get('DB_PASSWORD'),
+            };
+          default:
+            throw new Error('DB_TYPE not matched.');
+        }
+      },
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
